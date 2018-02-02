0c8916118f11a21d8e3315f850c8923541fc7f26
Theo Olausson <theoxolausson@gmail.com>
2018-02-02 14:44:37 +0000

    Fixed lighting and memory leaks
    Fixed lighting by reversing direction of vector between intersection and light
    Fixed definitive memory leaks by adding free statements in loop and to the light

--- source/Main.c
+++ source/Main.c
@@ -45,7 +45,7 @@ int main(int argc, char* argv[]) {
     /*
      * INITILIAZE OBJECTS IN IMAGE
      */
-    Vector3D* sphere1_centre = Vector3D_create(0.5, 0, 1);
+    Vector3D* sphere1_centre = Vector3D_create(0.5, 0, 2);
     Sphere* sphere1 = Sphere_create(sphere1_centre, 0.5, 150, 0, 0);
 
     Vector3D* sphere2_centre = Vector3D_create(-1, 1, 3);
@@ -54,8 +54,8 @@ int main(int argc, char* argv[]) {
     SpheresNode_add(sphere2);
     SpheresNode* spheres_traverser = spheres_tail;
 
-    Vector3D* light_centre = Vector3D_create(-1, 1, 0); //free me TODO
-    double light_luminance = 5;
+    Vector3D* light_centre = Vector3D_create(0, 1, -1); //free me TODO
+    double light_luminance = 10;
 
     /*
      * Dimensions of image in space = 2x2, centered at the <0, 0, 1>
@@ -73,9 +73,9 @@ int main(int argc, char* argv[]) {
             double y_coordinate = 1 - 2* ( (double) i / (double) height );
             
             // unit vector for the direction
-            Vector3D* ray_direction = Vector3D_create(x_coordinate, y_coordinate, 1);
-            //printf("Dir: %f\n", Vector3D_magnitude(ray_direction));
+            Vector3D* ray_direction = Vector3D_create(x_coordinate, y_coordinate, 1); 
             // ^(recall image is parallel to the plane but centered at <0, 0, 1>)
+            
             Vector3D* ray_origin = Vector3D_create(0, 0, 0);
 
             double t_min = 0;
@@ -99,7 +99,7 @@ int main(int argc, char* argv[]) {
                 }
 
 
-                // update t_min if t current t is closer to the camera
+                // update t_min if current t is closer to the camera (/origin)
                 if ( (t > 1 && t < t_min) || t_min == 0 ) {
                     t_min = t;
                     sphere_to_draw = SpheresNode_getSphere(spheres_traverser);
@@ -111,37 +111,31 @@ int main(int argc, char* argv[]) {
                 QuadraticSolution_destroy(quadratic_solution);
             }
 
-            if (t_min > 1) { //TODO is this correct?
+            if (t_min > 1) { 
                 Vector3D* intersection_point = Vector3D_multiply(ray_direction, t_min);
-                //printf("Ray_direction at: (%f, %f, %f)\n", Vector3D_getX(ray_direction), Vector3D_getY(ray_direction), Vector3D_getZ(ray_direction));
-                //printf("Intersection at: (%f, %f, %f)\n", Vector3D_getX(intersection_point), Vector3D_getY(intersection_point), Vector3D_getZ(intersection_point));
                 Vector3D* surface_normal = Vector3D_difference(intersection_point, Sphere_getCentre(sphere_to_draw));
-                Vector3D* light_to_intersection = Vector3D_difference(intersection_point, light_centre);
-                //printf("Surface_normal at: (%f, %f, %f)\n", Vector3D_getX(surface_normal), Vector3D_getY(surface_normal), Vector3D_getZ(surface_normal));
-                //printf("light_to_intersection at: (%f, %f, %f)\n\n", Vector3D_getX(light_to_intersection), Vector3D_getY(light_to_intersection), Vector3D_getZ(light_to_intersection));
+                Vector3D* intersection_to_light = Vector3D_difference(light_centre, intersection_point);
 
                 // cos(Theta) = a.b / (|a|*|b|)
-                //printf("Dot: %f\n", Vector3D_dot(surface_normal, light_to_intersection));
-                //printf("Magnitude of surface normal: %f\n", Vector3D_magnitude(surface_normal));
-                //printf("Magnitude of 'light to intersection': %f\n", Vector3D_magnitude(light_to_intersection));
-                double cos = Vector3D_dot(surface_normal, light_to_intersection)
-                                    / (Vector3D_magnitude(surface_normal) * Vector3D_magnitude(light_to_intersection));
-                //printf("cos: %f\n\n", cos);
+                double cos = Vector3D_dot(surface_normal, intersection_to_light)
+                                    / (Vector3D_magnitude(surface_normal) * Vector3D_magnitude(intersection_to_light));
                 cos = fmax(cos, 0);
 
-                double energy = light_luminance * cos / pow(Vector3D_magnitude(light_to_intersection), 1);
+                double energy = light_luminance * cos / pow(Vector3D_magnitude(intersection_to_light), 1.5);
                 double red = Sphere_getRed(sphere_to_draw) * energy;
                 if (red > 255) {red = 255;}
                 double green = Sphere_getGreen(sphere_to_draw) * energy;
                 if (green > 255) {green = 255;}
                 double blue = Sphere_getBlue(sphere_to_draw) * energy;
                 if (blue > 255) {blue = 255;}
-                //printf("Energy: %f\n", energy);
-                //printf("oldR: %u\n", Sphere_getRed(sphere_to_draw));
-                //printf("oldG: %u\n", Sphere_getGreen(sphere_to_draw));
-                //printf("oldB: %u\n", Sphere_getBlue(sphere_to_draw));
+
                 // re-initialize in new colour(s)
                 RGB_init(image_array[i][j], red, green, blue);
+
+                // Free allocated memory
+                Vector3D_destroy(intersection_point);
+                Vector3D_destroy(surface_normal);
+                Vector3D_destroy(intersection_to_light);
             }
 
             spheres_traverser = spheres_tail;
@@ -153,9 +147,10 @@ int main(int argc, char* argv[]) {
 
     }
 
-    //Destroy list  as it's now unused
+    // Destroy list  as it's now unused
     SpheresNode_destroyAllFollowing(spheres_tail);
-
+    Vector3D_destroy(light_centre);    
+    
     if (PPM_save(image_array, argv[1], height, width)) {
         printf("\nDone writing the file. Bye.\n========================\n\n");
     } else {
